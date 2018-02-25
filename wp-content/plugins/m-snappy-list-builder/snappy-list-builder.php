5d9e6492f7599e1123cdf52a3d4982af53f150eb
Mateus Mantovani <mateus@codebit.com.br>
2018-02-25 17:38:44 -0300

    Setting up Wordpress Plugin Headers

---
+++ wp-content/plugins/m-snappy-list-builder/snappy-list-builder.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin name: Snappy List Builder
+ * Plugin URI: http://unknow.temp
+ * Description: The ultimate email list building plugin for WordPress. Capture new subscribers. Reward subscribers with a custom download upon opt-in. Build unlimited lists. Import adn export subscribers easily with .csv
+ * Version: 1.0
+ * Author: Mateus Mantovani
+ * Author URI: http://temp.temp.temp
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: m-snappy-list-builder
+ *
+ */
