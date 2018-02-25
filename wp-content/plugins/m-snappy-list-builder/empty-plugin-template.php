163c3df8d5f5d92fd9acf7cc7f16858e94b480c4
Mateus Mantovani <mateus@codebit.com.br>
2018-02-25 17:48:12 -0300

    Template/Sample of a plugin file

---
+++ wp-content/plugins/m-snappy-list-builder/empty-plugin-template.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * THIS FILE IS JUST A SAMPLE AND IT ISN'T HAVE A UTILIZATION
+ * This is the template used in Course and shows how to divide the file of a plugin
+ */
+
+
+/* !0. TABLE OF CONTENTS */
+
+/*
+
+	1. HOOKS
+
+	2. SHORTCODES
+
+	3. FILTERS
+
+	4. EXTERNAL SCRIPTS
+
+	5. ACTIONS
+
+	6. HELPERS
+
+	7. CUSTOM POST TYPES
+
+	8. ADMIN PAGES
+
+	9. SETTINGS
+
+	10. MISCELLANEOUS
+
+*/
+
+
+
+
+/* !1. HOOKS */
+
+
+
+
+/* !2. SHORTCODES */
+
+
+
+
+/* !3. FILTERS */
+
+
+
+
+/* !4. EXTERNAL SCRIPTS */
+
+
+
+
+/* !5. ACTIONS */
+
+
+
+
+/* !6. HELPERS */
+
+
+
+
+/* !7. CUSTOM POST TYPES */
+
+
+
+
+/* !8. ADMIN PAGES */
+
+
+
+
+/* !9. SETTINGS */
+
+
+
+
+/* !10. MISCELLANEOUS */
+
+
+
