b60ed8c3c6efed6b42e5f5ce52abf7faadef88f6
Mateus Mantovani <mateus@codebit.com.br>
2018-07-26 23:22:50 -0300

    adicionando template

--- wp-content/plugins/m-snappy-list-builder/snappy-list-builder.php
+++ wp-content/plugins/m-snappy-list-builder/snappy-list-builder.php
@@ -774,8 +774,16 @@ function slb_confirm_subscription( $subscriber_id, $list_id ) {
 	// IF subscription was saved
 	if( $subscription_saved ):
 
-		// return true
-		$optin_complete = true;
+		// send email
+		$email_sent = slb_send_subscriber_email( $subscriber_id, 'subscription_confirmed', $list_id );
+
+		// IF email sent
+		if( $email_sent ):
+
+			// return true
+			$optin_complete = true;
+
+		endif;
 
 	endif;
 
@@ -1298,6 +1306,15 @@ function slb_get_email_template( $subscriber_id, $email_template_name, $list_id 
 					'. $default_email_header .'
 					<p>Thank you for subscribing to '. $list->post_title .'!</p>
 					<p>Please <a href="'. $optin_link .'">click here to confirm your subscription.</a></p>
+					'. $default_email_footer . $unsubscribe_text,
+		);
+
+		// template: subscription_confirmed
+		$email_templates['subscription_confirmed'] = array(
+			'subject' => 'You are now subscribed to '. $list->post_title .'!',
+			'body' => '
+					'. $default_email_header .'
+					<p>Thank you for confirming your subscription. You are now subscribed to '. $list->post_title .'!</p>
 					'. $default_email_footer . $unsubscribe_text,
 		);
 
